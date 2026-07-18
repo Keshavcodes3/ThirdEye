5c14492cec98d92b776351870ff06aebd03ee2b8
Keshavcodes3 <Keshavcodes0@gmail.com>
2026-07-18 15:22:55 +0530

    fix:fixing login middleare issue

--- Server/src/modules/auth/auth.controller.ts
+++ Server/src/modules/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import authutils from "./auth.utils.js";
 import { apiSuccessResponse } from "../../utils/ApiResponse.js";
 import jwt from 'jsonwebtoken'
 import { ENV } from "../../config/env.js";
+import { logger } from "../../config/logger.js";
 class authController {
     registerUser = asyncHandler(async (req: Request, res: Response) => {
         const { email, username, password } = req.body
@@ -18,15 +19,26 @@ class authController {
             username,
             password: hashedPassword
         })
+        if (!user) {
+            logger.error("user creation failed")
+            throw new ApiError(400, "user creation failed")
+        }
+        logger.success("user created successfully", user)
         return res.status(200).json(apiSuccessResponse(200, "User created successfully", user))
     })
 
     loginUser = asyncHandler(async (req: Request, res: Response) => {
         const { email, username, password } = req.body
         const user = await authRepository.findUserByEmail(email)
-        if (!user) throw new ApiError(404, "user not found")
+        if (!user) {
+            throw new ApiError(404, "user not found")
+            logger.error("user not found")
+        }
         const passwordMatched = await authutils.comparePassword(password, user.password)
-        if (!passwordMatched) throw new ApiError(400, "password not matched")
+        if (!passwordMatched) {
+            throw new ApiError(400, "password not matched")
+            logger.warn("password not matched")
+        }
         else {
             const token = jwt.sign({
                 userID: user._id,
@@ -34,15 +46,24 @@ class authController {
             }, ENV.JWT_ACCESS_SECRET, { expiresIn: "7d" })
             await authutils.setCookies(res, token)
         }
-        return res.status(200).json(apiSuccessResponse(200, "user logged in successfully",user))
+        logger.success("user logged in successfully", user)
+        return res.status(200).json(apiSuccessResponse(200, "user logged in successfully", user))
 
     })
 
     getUser = asyncHandler(async (req: Request, res: Response) => {
         const userId = (req as any).user.userID
-        if (!userId) throw new ApiError(400, "userID not found , log in first")
+        if (!userId) {
+            throw new ApiError(400, "userID not found , log in first")
+            logger.error("user not found")
+        }
+
         const user = await authRepository.findById(userId)
-        if (!user) throw new ApiError(404, "user not found")
+        if (!user) {
+            throw new ApiError(404, "user not found")
+
+        }
+        logger.success("user found successfully", user)
         return res.status(200).json(apiSuccessResponse(200, "user found successfully", user))
     })
 
